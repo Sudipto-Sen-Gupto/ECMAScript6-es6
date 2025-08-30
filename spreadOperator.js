2e38abc86319a51c24940bfd04f0345092e101a4
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2025-08-30 18:50:23 +0600

    spread operator added

---
+++ spreadOperator.js
@@ -0,0 +1,15 @@
+
+const collection=[12,23,34,54,65,76];
+// console.log(...collection);
+
+// const maximum=Math.max(20,92,400,892,1000,128);
+const maximum=Math.max(...collection)
+console.log(maximum);
+
+const first=[23,45,76,83,33];
+const second=[...first];
+second.unshift(11);
+console.log(second);
+
+const numbers=(a,b,...c)=>console.log(a,b,...c);
+numbers(13,25,18,90,23);
