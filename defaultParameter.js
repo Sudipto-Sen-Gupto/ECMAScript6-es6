52db66997b7e9acf280a81e2584c292cfc1933bf
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2025-08-30 18:22:42 +0600

    initial commit

---
+++ defaultParameter.js
@@ -0,0 +1,10 @@
+function sum(num1,num2=0){
+        console.log(num1,num2);
+    const result=num1+num2;
+    console.log(result);
+}
+// sum(10,20);
+function str(amarNam,tarNam=""){
+    console.log(amarNam,tarNam);
+}
+str("Gaurav");
