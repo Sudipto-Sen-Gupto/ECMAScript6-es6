60f9652ea607d2ebc6d417a91f4d8455a3d3f5f2
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2025-08-30 20:35:30 +0600

    entries added

---
+++ entries.js
@@ -0,0 +1,15 @@
+const market={Vagetable:"potato",price:60, quantity:1}
+
+const key=Object .keys(market);
+console.log(key);
+const value=Object.values(market);
+console.log(value);
+const all=Object.entries(market);
+console.log(all);
+
+Object.seal(market);
+const price=market.price;
+ market.type="Deshi"
+ market.price=price+10
+delete market.quantity;
+console.log(market);
